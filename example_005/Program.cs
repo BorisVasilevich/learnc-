ab08ae30c1e92202a3decf805b57d7b8c00843ff
BorisVasilevich <121186717+BorisVasilevich@users.noreply.github.com>
2023-01-12 15:01:51 +0300

    Задача про гири

---
+++ example_005/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 7;
+int b = 8;
+int c = 5;
+int d = 2;
+int e = 10;
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
