4449da1a035ecff2a887119fc9c017423ad98c54
BorisVasilevich <121186717+BorisVasilevich@users.noreply.github.com>
2023-01-12 23:00:15 +0300

    Задача с друзьми и собакой

---
+++ two_frends_dog/Program.cs
@@ -0,0 +1,29 @@
+﻿int speed_one_frend = 1;
+int speed_two_frend = 2;
+int speed_dog = 5;
+int distans = 10000;
+int count = 0;
+int frend = 1;
+int time = 0;
+
+while (distans>10)
+{
+    if (frend == 1)
+    {
+    time = distans/(speed_dog+speed_two_frend);
+    frend = 2;
+    }
+    if (frend == 2)
+    {
+    time = distans/(speed_dog+speed_one_frend);
+    frend = 1;
+    }
+distans = distans - time * (speed_one_frend+speed_two_frend);
+count+=1;
+
+
+}
+
+Console.Write("Собака пробежала ");
+Console.Write(count);
+Console.WriteLine(" раз!");
